ea03bfc36cba9152f69385a22b4b0376e56de90c
LeviCanales <milevi271@gmail.com>
2020-05-09 18:53:18 -0600

    Connecting and starting the database

---
+++ expense-tracker-back-end/Contexts/ExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using expense_tracker_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracker_back_end.Contexts
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ expense-tracker-back-end/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace expense_tracker_back_end.Models
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string correo { get; set; }
+    }
+}
